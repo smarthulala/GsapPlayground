efe44224d9a427cccd9c36488c7206bd13a5b16c
smarthulala <chao.long.au@gmail.com>
2024-03-03 00:14:21 +1100

    add bounded component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import gsap, { random } from 'gsap'
 import { useEffect, useRef } from 'react'
+import Bounded from './component/Bounded'
 
 export default function Home() {
   const component = useRef(null)
@@ -67,23 +68,23 @@ export default function Home() {
   }
 
   return (
-    <main
+    <Bounded
       className='flex min-h-screen flex-col items-center justify-between p-24'
       ref={component}
     >
       <div>
-        <h1 className='font-extrabold leading-none tracking-tighter text-nowrap'>
-          <span className='text-[clamp(2rem,15vmin,15rem)] block text-slate-500'>
+        <h1 className='text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter text-nowrap'>
+          <span className='block text-slate-500'>
             {renderLetters('PARORO', 'first')}
           </span>
-          <span className='text-[clamp(1rem,10vmin,10rem)] -mt-[.2em] block text-slate-400'>
+          <span className=' -mt-[.2em] block text-slate-400'>
             {renderLetters('KOREAN BBQ', 'second')}
           </span>
-          <span className='slogan text-[clamp(.5rem,5vmin,5rem)] block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold tracking-[.2em] text-transparent opacity-0 md:text-4xl'>
+          <span className='slogan block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold tracking-[.2em] text-transparent opacity-0 md:text-4xl'>
             ALL YOU CAN EAT
           </span>
         </h1>
       </div>
-    </main>
+    </Bounded>
   )
 }
